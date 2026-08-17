c6926e7fbfc6ef3f413e72f16d47f2a99cd0a7db
Juhainah Nasir <juhainah333@gmail.com>
2026-08-17 19:43:14 +0530

    fixes

--- src/routes/webhook.routes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const c = require('../controllers/webhook.controller');
-
-// Raw body parser is applied at app-level for this route
-router.post('/cashfree', c.cashfree);
-
-module.exports = router;
